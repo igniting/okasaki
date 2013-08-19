7dcb0751b305a36ea7b7c26170d5a8720eab0247
Anshu <anshu.avinash35@gmail.com>
2013-08-19 18:11:05 +0530

    Added commands for profiling;O(n^2) implementation

--- ch2/ex2_1.hs
+++ ch2/ex2_1.hs
@@ -1,4 +1,17 @@
+import System.Environment
+
 -- Function to return list of all suffixes of a list
+-- This is O(n^2) as confirmed by profiling
+
 suffixList :: [a] -> [[a]]
 suffixList [] = [[]]
 suffixList l@(_:xs) = l:suffixList xs
+
+main = do
+  args <- getArgs
+  if length args /= 1
+    then putStrLn "One argument required."
+    else do
+      let l = suffixList [1..n] where n = read $ head args :: Int
+      putStrLn $ show l
+      putStrLn "Suffix List Calculated."
