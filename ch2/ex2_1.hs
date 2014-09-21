840629efc6932b8c14758cb4234462effea2a88c
Anshu Avinash <anshu.avinash35@gmail.com>
2014-09-21 13:47:43 +0530

    Calculate just the length of list
    Reducing the entire list to the normal form is O(n^2).

--- ch2/ex2_1.hs
+++ ch2/ex2_1.hs
@@ -1,14 +1,13 @@
 import Criterion.Main
 
 -- Function to return list of all suffixes of a list
--- This function is O(n), confirmed by profiling
 suffixList :: [Int] -> [[Int]]
 suffixList [] = [[]]
 suffixList l@(_:xs) = l:suffixList xs
 
 main = defaultMain [
         bgroup "suffixList" [
-          bench "suffixList [1..100]" $ nf suffixList [1..100]
-        , bench "suffixList [1..1000]" $ nf suffixList [1..1000]
+          bench "suffixList [1..100]" $ nf length $ suffixList [1..100]
+        , bench "suffixList [1..1000]" $ nf length $ suffixList [1..1000]
         ]
       ]
